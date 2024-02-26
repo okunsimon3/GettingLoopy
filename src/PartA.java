e0ce990de5252ff512ae1ea4c1c0e08388670964
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2024-02-26 16:37:04 -0500

    Initial Commit

---
+++ src/PartA.java
@@ -0,0 +1,19 @@
+public class PartA {
+    public static void main(String[] args) {
+        for(int cnt = 0; cnt <= 30; cnt++) {
+            System.out.println(cnt);
+        }
+        System.out.println("\n");
+        for(int cnt = 30; cnt >= 0; cnt--) {
+            System.out.println(cnt);
+        }
+        System.out.println("\n");
+        for(int cnt= 0; cnt <= 18;cnt += 3) {
+            System.out.println(cnt);
+        }
+        System.out.println("\n");
+        for(int cnt = 10; cnt >=0; cnt -= 2) {
+            System.out.println(cnt);
+        }
+    }
+}
